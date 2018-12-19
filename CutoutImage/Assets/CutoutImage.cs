e4f8468f658e0f88b1f4ed187948410e0bf3bbd0
Hirayama Takashi <hirayama-takashi@kayac.com>
2018-12-19 16:54:02 +0900

    UI測定サンプル変更

---
+++ CutoutImage/Assets/CutoutImage.cs
@@ -0,0 +1,283 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Kayac
+{
+	// 上流のコンポーネントのscaleのzに0が入ってると動作がおかしくなる(原因不明)
+	public class CutoutImage : MaskableGraphic
+	{
+		[SerializeField]
+		private Sprite _sprite;
+		public Sprite sprite
+		{
+			get
+			{
+				return _sprite;
+			}
+		}
+
+		[SerializeField]
+		private Vector2[] _overrideVertices;
+
+		public Vector2[] overrideVertices
+		{
+			get { return _overrideVertices; }
+		}
+
+		public override Texture mainTexture
+		{
+			get
+			{
+				return (_sprite != null) ? _sprite.texture : null;
+			}
+		}
+
+		public void ReplaceSprite(Sprite sprite)
+		{
+			_sprite = sprite;
+		}
+
+		public override void SetNativeSize()
+		{
+			if (_sprite == null)
+			{
+				Debug.LogWarning("sprite刺さってないのでできない");
+				return;
+			}
+			if ((rectTransform.anchorMin.x != rectTransform.anchorMax.x)
+				|| (rectTransform.anchorMin.y != rectTransform.anchorMax.y))
+			{
+				Debug.LogWarning("自動伸縮あるとできない");
+				return;
+			}
+			Debug.Assert(!_sprite.packed
+				|| ((_sprite.packingMode == SpritePackingMode.Rectangle) && (_sprite.packingRotation == SpritePackingRotation.None)));
+			var srcRect = (_sprite.packed) ? _sprite.textureRect : _sprite.rect;
+			rectTransform.sizeDelta = new Vector2(srcRect.width, srcRect.height);
+		}
+
+		protected override void OnPopulateMesh(VertexHelper vh)
+		{
+			if (_sprite == null)
+			{
+				return;
+			}
+			vh.Clear();
+			Color32 color32 = this.color;
+			var dstSizeDelta = rectTransform.sizeDelta;
+			var dstPivot = rectTransform.pivot;
+			Debug.Assert(!_sprite.packed
+				|| ((_sprite.packingMode == SpritePackingMode.Rectangle) && (_sprite.packingRotation == SpritePackingRotation.None)));
+			var srcRect = (_sprite.packed) ? _sprite.textureRect : _sprite.rect;
+			if ((_overrideVertices != null) && (_overrideVertices.Length >= 3))
+			{
+				var toUvOffsetX = dstPivot.x;
+				var toUvOffsetY = dstPivot.y;
+				Vector2 uv;
+				if (_sprite.packed)
+				{
+					var spriteRect = _sprite.rect;
+					var rcpAtlasWidth = 1f / _sprite.texture.width;
+					var rcpAtlasHeight = 1f / _sprite.texture.height;
+					for (int i = 0; i < _overrideVertices.Length; i++)
+					{
+						var pos = _overrideVertices[i];
+						uv.x = (srcRect.xMin + spriteRect.width * (pos.x + toUvOffsetX)) * rcpAtlasWidth;
+						uv.y = (srcRect.yMin + spriteRect.height * (pos.y + toUvOffsetY)) * rcpAtlasHeight;
+						pos.x *= dstSizeDelta.x;
+						pos.y *= dstSizeDelta.y;
+						vh.AddVert(pos, color32, uv);
+					}
+				}
+				else
+				{
+					for (int i = 0; i < _overrideVertices.Length; i++)
+					{
+						var pos = _overrideVertices[i];
+						uv.x = pos.x;
+						uv.y = pos.y;
+						uv.x += toUvOffsetX;
+						uv.y += toUvOffsetY;
+						pos.x *= dstSizeDelta.x;
+						pos.y *= dstSizeDelta.y;
+						vh.AddVert(pos, color32, uv);
+					}
+				}
+				for (int i = 2; i < _overrideVertices.Length; i++)
+				{
+					vh.AddTriangle(0, i - 1, i);
+				}
+			}
+			else
+			{
+				var positions = _sprite.vertices;
+				var uvs = _sprite.uv;
+				var indices = _sprite.triangles;
+				Debug.Assert(positions.Length == uvs.Length);
+				var rcpSrcWidth = 1f / srcRect.width;
+				var rcpSrcHeight = 1f / srcRect.height;
+				var scaleX = _sprite.pixelsPerUnit * dstSizeDelta.x * rcpSrcWidth;
+				var scaleY = _sprite.pixelsPerUnit * dstSizeDelta.y * rcpSrcHeight;
+				var srcPivot = _sprite.pivot;
+				srcPivot.x *= rcpSrcWidth; // [0,1]に正規化
+				srcPivot.y *= rcpSrcHeight;
+				var offsetX = (srcPivot.x - dstPivot.x) * dstSizeDelta.x;
+				var offsetY = (srcPivot.y - dstPivot.y) * dstSizeDelta.y;
+				for (int i = 0; i < positions.Length; i++)
+				{
+					var pos = positions[i];
+					pos.x *= scaleX;
+					pos.y *= scaleY;
+					pos.x += offsetX;
+					pos.y += offsetY;
+					vh.AddVert(pos, color32, uvs[i]);
+				}
+				for (int i = 0; i < indices.Length; i += 3)
+				{
+					vh.AddTriangle(indices[i + 0], indices[i + 1], indices[i + 2]);
+				}
+			}
+		}
+
+#if UNITY_EDITOR
+		private void OnDrawGizmosSelected()
+		{
+			if (_sprite == null)
+			{
+				return;
+			}
+			var ap = rectTransform.anchoredPosition;
+			var t = rectTransform.localToWorldMatrix;
+			Gizmos.color = new Color(0f, 1f, 0f, 1f);
+			if ((_overrideVertices != null) && (_overrideVertices.Length >= 3))
+			{
+				var dstSizeDelta = rectTransform.sizeDelta;
+				var v = _overrideVertices[_overrideVertices.Length - 1];
+				v.x *= dstSizeDelta.x;
+				v.y *= dstSizeDelta.y;
+				var prev = t.MultiplyPoint3x4(v);
+				for (int i = 0; i < _overrideVertices.Length; i++)
+				{
+					v = _overrideVertices[i];
+					v.x *= dstSizeDelta.x;
+					v.y *= dstSizeDelta.y;
+					var current = t.MultiplyPoint3x4(v);
+					Gizmos.DrawLine(prev, current);
+					prev = current;
+				}
+			}
+			else
+			{
+				Debug.Assert(!_sprite.packed
+					|| ((_sprite.packingMode == SpritePackingMode.Rectangle) && (_sprite.packingRotation == SpritePackingRotation.None)));
+				var srcRect = (_sprite.packed) ? _sprite.textureRect : _sprite.rect;
+				var rcpSrcWidth = 1f / srcRect.width;
+				var rcpSrcHeight = 1f / srcRect.height;
+				var dstSizeDelta = rectTransform.sizeDelta;
+				var dstPivot = rectTransform.pivot;
+				var scaleX = _sprite.pixelsPerUnit * dstSizeDelta.x * rcpSrcWidth;
+				var scaleY = _sprite.pixelsPerUnit * dstSizeDelta.y * rcpSrcHeight;
+				var srcPivot = _sprite.pivot;
+				srcPivot.x *= rcpSrcWidth; // [0,1]に正規化
+				srcPivot.y *= rcpSrcHeight;
+				var offsetX = (srcPivot.x - dstPivot.x) * dstSizeDelta.x;
+				var offsetY = (srcPivot.y - dstPivot.y) * dstSizeDelta.y;
+				var positions = _sprite.vertices;
+				var indices = _sprite.triangles;
+				// TODO: 座標変換3倍通しててマジ重いが、変換済み頂点列前もって作るとメモリ汚すのでやってない。3倍くらいならいいかなあ。
+				for (int i = 0; i < indices.Length; i += 3)
+				{
+					var p0 = positions[indices[i + 0]];
+					var p1 = positions[indices[i + 1]];
+					var p2 = positions[indices[i + 2]];
+					p0.x *= scaleX;
+					p1.x *= scaleX;
+					p2.x *= scaleX;
+					p0.y *= scaleY;
+					p1.y *= scaleY;
+					p2.y *= scaleY;
+					p0.x += offsetX;
+					p1.x += offsetX;
+					p2.x += offsetX;
+					p0.y += offsetY;
+					p1.y += offsetY;
+					p2.y += offsetY;
+					p0 = t.MultiplyPoint3x4(p0);
+					p1 = t.MultiplyPoint3x4(p1);
+					p2 = t.MultiplyPoint3x4(p2);
+					Gizmos.DrawLine(p0, p1);
+					Gizmos.DrawLine(p1, p2);
+					Gizmos.DrawLine(p2, p0);
+				}
+			}
+		}
+
+		[CustomEditor(typeof(CutoutImage), true)]
+		public class ShavedImageInspector : Editor
+		{
+			public override void OnInspectorGUI()
+			{
+				base.OnInspectorGUI();
+				var self = (CutoutImage)target;
+
+				if (GUILayout.Button("Set Native Size"))
+				{
+					self.SetNativeSize();
+				}
+			}
+
+			private void OnSceneGUI()
+			{
+				Tools.current = Tool.None;
+
+				var component = target as CutoutImage;
+				var rectTransform = component.rectTransform;
+				var overrideVertices = component._overrideVertices;
+
+				var t = rectTransform.localToWorldMatrix;
+				var inverse = rectTransform.worldToLocalMatrix;
+				if ((overrideVertices != null) && (overrideVertices.Length >= 3))
+				{
+					var dstSizeDelta = rectTransform.sizeDelta;
+					var v = overrideVertices[overrideVertices.Length - 1];
+					v.x *= dstSizeDelta.x;
+					v.y *= dstSizeDelta.y;
+					for (int i = 0; i < overrideVertices.Length; i++)
+					{
+						v = overrideVertices[i];
+						v.x *= dstSizeDelta.x;
+						v.y *= dstSizeDelta.y;
+						var current = t.MultiplyPoint3x4(v);
+						PositionHandle(ref overrideVertices[i], ref inverse, ref current, ref dstSizeDelta);
+					}
+
+					component.SetVerticesDirty();
+				}
+			}
+
+			void PositionHandle(ref Vector2 targetPoint, ref Matrix4x4 inverse, ref Vector3 position, ref Vector2 sizeDelta)
+			{
+				// TODO: rotationも考慮していい感じにする
+				var handleSize = HandleUtility.GetHandleSize(position) * 0.2f;
+				var newWorldPosition = Handles.FreeMoveHandle(position, Quaternion.identity, handleSize, new Vector3(1f, 1f, 0f), Handles.CircleHandleCap);
+				var newPosition = inverse.MultiplyPoint3x4(newWorldPosition);
+				newPosition.x /= sizeDelta.x;
+				newPosition.y /= sizeDelta.y;
+				if (Mathf.Abs(newPosition.x - targetPoint.x) > 1e-5f)
+				{
+					targetPoint.x = newPosition.x;
+				}
+				if (Mathf.Abs(newPosition.y - targetPoint.y) > 1e-5f)
+				{
+					targetPoint.y = newPosition.y;
+				}
+			}
+		}
+#endif
+	}
+}
--- RichTextVertices/Assets/Sample.cs
+++ RichTextVertices/Assets/Sample.cs
@@ -7,27 +7,113 @@ public class Sample : MonoBehaviour
 {
 	public Text textPrefab;
 	public Image imagePrefab;
+	public Sprite[] sprites;
 	public Canvas canvas;
-	public bool setAll;
-	public bool setText;
-	public bool setGraphicColor;
-	public bool setCanvasRendererColor;
-	public bool setCanvasRendererAlpha;
-	public bool setPosition;
-	public bool setRotation;
-	public bool setScale;
-	public bool setSizeDelta;
-	public bool useRichText;
-	public bool insertImage;
 	public int graphicCount = 100;
+	public int spriteCount = int.MaxValue;
 
+	bool _setAll = true;
+	bool _setText;
+	bool _setGraphicColor;
+	bool _setCanvasRendererColor;
+	bool _setCanvasRendererAlpha;
+	bool _setPosition;
+	bool _setRotation;
+	bool _setScale;
+	bool _setSizeDelta;
+	bool _useRichText = true;
+	bool _insertImage = true;
+	bool _outLine = true;
+	bool _degenerationRemoval = true;
+
+	bool _uiEnabled = true;
 	Graphic[] _graphics;
 	int _frame;
+
+	void OnGUI()
+	{
+		if (!_uiEnabled)
+		{
+			return;
+		}
+		if (GUILayout.Toggle(_setAll, "setAll") != _setAll)
+		{
+			_setAll = !_setAll;
+		}
+		if (GUILayout.Toggle(_setText, "setText") != _setText)
+		{
+			_setText = !_setText;
+		}
+		if (GUILayout.Toggle(_setGraphicColor, "setGraphicColor") != _setGraphicColor)
+		{
+			_setGraphicColor = !_setGraphicColor;
+		}
+		if (GUILayout.Toggle(_setCanvasRendererColor, "setCanvasRendererColor") != _setCanvasRendererColor)
+		{
+			_setCanvasRendererColor = !_setCanvasRendererColor;
+		}
+		if (GUILayout.Toggle(_setCanvasRendererAlpha, "setCanvasRendererAlpha") != _setCanvasRendererAlpha)
+		{
+			_setCanvasRendererAlpha = !_setCanvasRendererAlpha;
+		}
+		if (GUILayout.Toggle(_setPosition, "setPosition") != _setPosition)
+		{
+			_setPosition = !_setPosition;
+		}
+		if (GUILayout.Toggle(_setRotation, "setRotation") != _setRotation)
+		{
+			_setRotation = !_setRotation;
+		}
+		if (GUILayout.Toggle(_setScale, "setScale") != _setScale)
+		{
+			_setScale = !_setScale;
+		}
+		if (GUILayout.Toggle(_setSizeDelta, "setSizeDelta") != _setSizeDelta)
+		{
+			_setSizeDelta = !_setSizeDelta;
+		}
+		if (GUILayout.Toggle(_useRichText, "useRichText") != _useRichText)
+		{
+			_useRichText = !_useRichText;
+			Reset();
+		}
+		if (GUILayout.Toggle(_insertImage, "insertImage") != _insertImage)
+		{
+			_insertImage = !_insertImage;
+			Reset();
+		}
+		if (GUILayout.Toggle(_outLine, "outLine") != _outLine)
+		{
+			_outLine = !_outLine;
+			foreach (var graphic in _graphics)
+			{
+				var text = graphic as Text;
+				if (text != null)
+				{
+					var component = text.gameObject.GetComponent<Outline>();
+					component.enabled = _outLine;
+				}
+			}
+		}
+		if (GUILayout.Toggle(_degenerationRemoval, "degenerationRemoval") != _degenerationRemoval)
+		{
+			_degenerationRemoval = !_degenerationRemoval;
+			foreach (var graphic in _graphics)
+			{
+				var text = graphic as Text;
+				if (text != null)
+				{
+					var remover = text.gameObject.GetComponent<Kayac.DegenerateQuadRemover>();
+					remover.enabled = _degenerationRemoval;
+				}
+			}
+		}
+	}
 
 	void Start()
 	{
 		string str = null;
-		if (useRichText)
+		if (_useRichText)
 		{
 			str = "<color=#ff0000>赤</color>";
 		}
@@ -38,9 +124,10 @@ public class Sample : MonoBehaviour
 
 		_graphics = new Graphic[graphicCount];
 		var parent = canvas.gameObject.transform;
+		var spriteIndex = 0;
 		for (int i = 0; i < graphicCount; i++)
 		{
-			if (!insertImage || ((i % 2) == 0))
+			if (!_insertImage || ((i % 2) == 0))
 			{
 				var text = Instantiate(textPrefab, parent, false);
 				text.text = str;
@@ -48,42 +135,43 @@ public class Sample : MonoBehaviour
 			}
 			else
 			{
-				_graphics[i] = Instantiate(imagePrefab, parent, false);
+				var image = Instantiate(imagePrefab, parent, false);
+				image.sprite = sprites[spriteIndex];
+				_graphics[i] = image;
+				spriteIndex++;
+				if ((spriteIndex >= spriteCount) || (spriteIndex >= sprites.Length))
+				{
+					spriteIndex = 0;
+				}
 			}
 		}
 	}
+	void OnDestroy()
+	{
+		if (_graphics != null)
+		{
+			foreach (var item in _graphics)
+			{
+				Destroy(item.gameObject);
+			}
+			_graphics = null;
+		}
+	}
+	void Reset()
+	{
+		OnDestroy();
+		Start();
+	}
 	void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.F1)) // Outlineの有効無効
+		if (Input.GetKeyDown(KeyCode.F1))
 		{
-			foreach (var graphic in _graphics)
-			{
-				var text = graphic as Text;
-				if (text != null)
-				{
-					var outline = text.gameObject.GetComponent<Outline>();
-					outline.enabled = !outline.enabled;
-				}
-			}
+			_uiEnabled = !_uiEnabled;
 		}
-
-		if (Input.GetKeyDown(KeyCode.F2)) // DegenerateQuadRemoverの有効無効
-		{
-			foreach (var graphic in _graphics)
-			{
-				var text = graphic as Text;
-				if (text != null)
-				{
-					var remover = text.gameObject.GetComponent<Kayac.DegenerateQuadRemover>();
-					remover.enabled = !remover.enabled;
-				}
-			}
-		}
-
 		string str = null;
-		if (setText)
+		if (_setText)
 		{
-			if (useRichText)
+			if (_useRichText)
 			{
 				str = "<color=#ff0000>" + _frame.ToString("D5") + "</color>";
 			}
@@ -95,14 +183,14 @@ public class Sample : MonoBehaviour
 		float alpha = UnityEngine.Random.Range(0f, 1f);
 		Color color = new Color(1f, 1f, 1f, alpha);
 
-		int count = setAll ? _graphics.Length : 1;
+		int count = _setAll ? _graphics.Length : 1;
 
 		for (int i = 0; i < count; i++)
 		{
 			var graphic = _graphics[i];
 			var transform = graphic.rectTransform;
 
-			if (setText)
+			if (_setText)
 			{
 				var text = graphic as Text;
 				if (text != null)
@@ -111,12 +199,12 @@ public class Sample : MonoBehaviour
 				}
 			}
 
-			if (setGraphicColor)
+			if (_setGraphicColor)
 			{
 				graphic.color = color;
 			}
 
-			if (setCanvasRendererColor)
+			if (_setCanvasRendererColor)
 			{
 				var renderer = graphic.gameObject.GetComponent<CanvasRenderer>();
 				if (renderer != null)
@@ -125,7 +213,7 @@ public class Sample : MonoBehaviour
 				}
 			}
 
-			if (setCanvasRendererAlpha)
+			if (_setCanvasRendererAlpha)
 			{
 				var renderer = graphic.gameObject.GetComponent<CanvasRenderer>();
 				if (renderer != null)
@@ -134,7 +222,7 @@ public class Sample : MonoBehaviour
 				}
 			}
 
-			if (setPosition)
+			if (_setPosition)
 			{
 				var pos = transform.anchoredPosition;
 				pos.x = UnityEngine.Random.Range(-320f, 320f);
@@ -142,14 +230,14 @@ public class Sample : MonoBehaviour
 				transform.anchoredPosition = pos;
 			}
 
-			if (setRotation)
+			if (_setRotation)
 			{
 				var rot = transform.localRotation;
 				rot = Quaternion.Euler(0f, 0f, UnityEngine.Random.Range(0f, 360f));
 				transform.localRotation = rot;
 			}
 
-			if (setScale)
+			if (_setScale)
 			{
 				var scale = transform.localScale;
 				scale.x = UnityEngine.Random.Range(0f, 1f);
@@ -157,7 +245,7 @@ public class Sample : MonoBehaviour
 				transform.localScale = scale;
 			}
 
-			if (setSizeDelta)
+			if (_setSizeDelta)
 			{
 				var size = transform.sizeDelta;
 				size.x = UnityEngine.Random.Range(50f, 300f);
