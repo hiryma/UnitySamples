e8f816cbc28d57403d4208b784b97be7dcbea474
Hirayama Takashi <hirayama-takashi@kayac.com>
2019-01-21 13:25:26 +0900

    ボタンとポップアップ

---
+++ InterpolationFunctions/Assets/SampleUI/ButtonAnimation.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class ButtonAnimation : MonoBehaviour
+	, IPointerExitHandler
+	, IPointerDownHandler
+	, IPointerUpHandler
+{
+	[SerializeField]
+	float _spring = 100f;
+	[SerializeField]
+	float _damper = 15f;
+	[SerializeField]
+	float _fireV0 = 15f;
+	[SerializeField]
+	float _downGoalScale = 0.8f;
+	[SerializeField]
+	UnityEvent _onClick;
+
+	float _velocity;
+	float _goal = 1f;
+	bool _down;
+
+	public void OnPointerExit(PointerEventData data)
+	{
+		_down = false;
+		StartRelax();
+	}
+
+	public void OnPointerDown(PointerEventData data)
+	{
+		_down = true;
+		StartDown();
+	}
+
+	public void OnPointerUp(PointerEventData data)
+	{
+		if (_down)
+		{
+			StartFire();
+			if (_onClick != null)
+			{
+				_onClick.Invoke();
+			}
+		}
+		_down = false;
+	}
+
+	void StartDown()
+	{
+		_goal = _downGoalScale;
+	}
+
+	void StartRelax()
+	{
+		_goal = 1f;
+	}
+
+	void StartFire()
+	{
+		_goal = 1f;
+		_velocity = _fireV0;
+	}
+
+	void Update()
+	{
+		var localScale = gameObject.transform.localScale;
+		var current = localScale.x;
+		var a = ((_goal - current) * _spring) - (_velocity * _damper);
+		var dt = Time.deltaTime;
+		_velocity += a * dt;
+		current += _velocity * dt;
+		gameObject.transform.localScale = new Vector3(current, current, 1f);
+	}
+}
---
+++ InterpolationFunctions/Assets/SampleUI/PopupAnimation.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupAnimation : MonoBehaviour
+{
+	[SerializeField]
+	float _spring = 100f;
+	[SerializeField]
+	float _openDamper = 14f;
+	[SerializeField]
+	float _closeDamper = 15f;
+	[SerializeField]
+	Vector2 _openV0 = new Vector2(10f, 10f);
+	[SerializeField]
+	Vector2 _closeV0 = new Vector2(-7f, -7f);
+
+	Vector2 _goal;
+	Vector2 _velocity;
+	float _damper;
+
+	void Awake()
+	{
+		Close();
+		gameObject.transform.localScale = new Vector3(0f, 0f, 1f);
+		_velocity = Vector2.zero;
+	}
+
+	public void Open()
+	{
+		_goal = new Vector2(1f, 1f);
+		_velocity = _openV0;
+		_damper = _openDamper;
+	}
+
+	public void Close()
+	{
+		_goal = new Vector2(0f, 0f);
+		_velocity = _closeV0;
+		_damper = _closeDamper;
+	}
+
+	public void Toggle()
+	{
+		if (_goal.y > 0f)
+		{
+			Close();
+		}
+		else
+		{
+			Open();
+		}
+	}
+
+	void Update()
+	{
+		var transform = gameObject.transform;
+		var localScale = transform.localScale;
+		var current = new Vector2(localScale.x, localScale.y);
+		var a = ((_goal - current) * _spring) - (_velocity * _damper);
+		var dt = Time.deltaTime;
+		_velocity += a * dt;
+		current += _velocity * dt;
+		transform.localScale = new Vector3(current.x, current.y, 1f);
+	}
+}
