ab5ebc16c4f3a04d2c7984914b499fc7209048b7
Hirayama Takashi <hirayama-takashi@kayac.com>
2019-02-04 13:34:36 +0900

    コード直し

--- CompileTime/Assets/Editor/CompileTimer.cs
+++ CompileTime/Assets/Editor/CompileTimer.cs
@@ -4,6 +4,7 @@ using System;
 
 namespace Kayac
 {
+
 	public class CompileTimer : EditorWindow
 	{
 		[MenuItem("Kayac/CompileTimer")]
@@ -14,42 +15,50 @@ namespace Kayac
 		}
 
 		// コンパイル前後で変数を保持できないのでEditorPrefsに入れる必要がある
-		const string prevCompilingKey = "kayac_prevCompiling";
 		const string compileStartTimeKey = "kayac_compileStartTime";
 		const string lastCompileTimeKey = "kayac_lastCompileTime";
 
 		void OnGUI()
 		{
+			DateTime startTime = new DateTime();
 			var compiling = EditorApplication.isCompiling;
-			bool prevCompiling = EditorPrefs.GetBool(prevCompilingKey, false);
-			int compileStartTime = EditorPrefs.GetInt(compileStartTimeKey, 0);
-			float lastCompileTime = EditorPrefs.GetFloat(lastCompileTimeKey, 0f);
-			var baseTime = new DateTime(2019, 1, 1);
-			var currentCompileTime = 0f;
+			bool prevCompiling = false;
+			if (EditorPrefs.HasKey(compileStartTimeKey))
+			{
+				string str = EditorPrefs.GetString(compileStartTimeKey);
+				if (!String.IsNullOrEmpty(str))
+				{
+					startTime = DateTime.Parse(str);
+					prevCompiling = true;
+				}
+			}
+			float lastTime = 0f;
+			lastTime = EditorPrefs.GetFloat(lastCompileTimeKey);
+			var currentTime = 0f;
 			if (prevCompiling)
 			{
-				var startTime = baseTime.AddSeconds(compileStartTime);
+				currentTime = (float)(DateTime.Now - startTime).TotalSeconds;
 				if (compiling)
 				{
-					currentCompileTime = (float)(DateTime.Now - startTime).TotalSeconds;
+					currentTime = (float)(DateTime.Now - startTime).TotalSeconds;
 				}
 				else
 				{
-					lastCompileTime = (float)(DateTime.Now - startTime).TotalSeconds;
-					EditorPrefs.SetFloat(lastCompileTimeKey, lastCompileTime);
-					EditorPrefs.SetInt(compileStartTimeKey, 0);
+					lastTime = (float)(DateTime.Now - startTime).TotalSeconds;
+					EditorPrefs.SetFloat(lastCompileTimeKey, lastTime);
+					EditorPrefs.DeleteKey(compileStartTimeKey);
 				}
 			}
 			else if (compiling)
 			{
-				var startTime = (int)(DateTime.Now - baseTime).TotalSeconds;
-				EditorPrefs.SetInt(compileStartTimeKey, startTime);
+				EditorPrefs.SetString(compileStartTimeKey, DateTime.Now.ToString());
 			}
-
-			EditorGUILayout.LabelField("Compiling:", compiling ? currentCompileTime.ToString("F2") : "No");
-			EditorGUILayout.LabelField("LastCompileTime:", lastCompileTime.ToString("F2"));
-			EditorPrefs.SetBool(prevCompilingKey, compiling);
-
+			else if (EditorPrefs.HasKey(compileStartTimeKey))
+			{
+				EditorPrefs.DeleteKey(compileStartTimeKey);
+			}
+			EditorGUILayout.LabelField("Compiling:", compiling ? currentTime.ToString("F2") : "No");
+			EditorGUILayout.LabelField("LastCompileTime:", lastTime.ToString("F2"));
 			this.Repaint();
 		}
 	}
