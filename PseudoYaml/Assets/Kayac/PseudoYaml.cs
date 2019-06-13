38ee213208d6a32f698143c4cf4720d0e0f34fde
Hirayama Takashi <hirayama-takashi@kayac.com>
2019-06-14 00:16:40 +0900

    シリアライザだけ

---
+++ PseudoYaml/Assets/Kayac/PseudoYaml.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Reflection;
+
+public static class PseudoYaml
+{
+	public class SerializeFieldAttribute : System.Attribute
+	{
+
+	}
+
+	public static string Serialize(object root)
+	{
+		Debug.Assert(root != null);
+		var sb = new System.Text.StringBuilder();
+		var type = root.GetType();
+		var fields = GetFields(type);
+		foreach (var field in fields)
+		{
+			if (IsSerialized(field))
+			{
+				var child = field.GetValue(root);
+				Serialize(sb, child, field.Name, field.FieldType, 0, false);
+			}
+		}
+		return sb.ToString();
+	}
+
+	static void Serialize(
+		System.Text.StringBuilder sb,
+		object o,
+		string fieldName,
+		Type type,
+		int level,
+		bool arrayElement)
+	{
+		sb.Append(' ', level * 2);
+		if (arrayElement)
+		{
+			sb.Append("- ");
+		}
+		if (o == null)
+		{
+			Debug.Assert(fieldName != null);
+			sb.AppendFormat("{0}: null\n", fieldName);
+		}
+		else if (type.IsPrimitive || (type == typeof(string)))
+		{
+			sb.AppendFormat("{0}: {1}\n", fieldName, o.ToString());
+		}
+		else if (type.IsArray)
+		{
+			sb.AppendFormat("{0}:\n", fieldName);
+			var array = o as Array;
+			var elementType = type.GetElementType();
+			foreach (var item in array)
+			{
+				Serialize(sb, item, null, elementType, level + 1, true);
+			}
+		}
+		else if (type.IsGenericType && (type.GetGenericTypeDefinition() == typeof(List<>)))
+		{
+			sb.AppendFormat("{0}:\n", fieldName);
+			var list = o as IList;
+			foreach (var item in list)
+			{
+				Serialize(sb, item, null, item.GetType(), level + 1, true);
+			}
+		}
+		else
+		{
+			if (!string.IsNullOrEmpty(fieldName))
+			{
+				sb.AppendFormat("{0}:", fieldName);
+			}
+			sb.Append('\n');
+			var fields = GetFields(type);
+			foreach (var field in fields)
+			{
+				if (IsSerialized(field))
+				{
+					var child = field.GetValue(o);
+					Serialize(sb, child, field.Name, field.FieldType, level + 1, false);
+				}
+			}
+		}
+	}
+
+	// 後でいじりそうなので出しておく
+	static bool IsSerialized(FieldInfo field)
+	{
+		return (!field.IsNotSerialized);
+	}
+
+	static FieldInfo[] GetFields(Type type)
+	{
+		var fields = type.GetFields(
+			BindingFlags.Instance
+			| BindingFlags.Public
+			| BindingFlags.NonPublic);
+		return fields;
+	}
+
+	public static T Deserialize<T>(string yaml) where T: class, new()
+	{
+		return null;
+	}
+}
---
+++ PseudoYaml/Assets/Main.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.IO;
+
+public class Main : MonoBehaviour
+{
+	[SerializeField]
+	Text text;
+
+	class Node
+	{
+		public int id;
+		public string name;
+		public List<Node> children;
+		[System.NonSerialized]
+		float magic = 1.2345f; // こいつは出てこないはず
+	}
+	Node root;
+	string yaml;
+	bool showYaml;
+	int nextId = 0;
+
+	void Start()
+	{
+		var dir = Application.dataPath;
+		root = new Node();
+		root.name = dir;
+		root.id = nextId;
+		nextId++;
+
+		// root構築
+		Scan(root);
+
+		var sb = new System.Text.StringBuilder();
+		ToString(sb, root, 0);
+		text.text = sb.ToString();
+	}
+
+	void Scan(Node node)
+	{
+		var dirs = Directory.GetDirectories(node.name);
+		if (dirs.Length > 0)
+		{
+			node.children = new List<Node>();
+			foreach (var dir in dirs)
+			{
+				var child = new Node();
+				child.name = dir;
+				child.id = nextId;
+				nextId++;
+				node.children.Add(child);
+				Scan(child);
+			}
+		}
+		var files = Directory.GetFiles(node.name);
+		if (files.Length > 0)
+		{
+			if (node.children == null)
+			{
+				node.children = new List<Node>();
+			}
+			foreach (var file in files)
+			{
+				var child = new Node();
+				child.name = file;
+				child.id = nextId;
+				nextId++;
+				node.children.Add(child);
+			}
+		}
+	}
+
+	void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			showYaml = !showYaml;
+			if (showYaml)
+			{
+				yaml = PseudoYaml.Serialize(root);
+				Debug.Log(yaml);
+				text.text = yaml;
+			}
+			else
+			{
+				root = PseudoYaml.Deserialize<Node>(yaml);
+				if (root == null)
+				{
+					text.text = "BUG!!! Deserialization Failed!!!";
+				}
+				else
+				{
+					var sb = new System.Text.StringBuilder();
+					ToString(sb, root, 0);
+					text.text = sb.ToString();
+				}
+			}
+		}
+	}
+
+	void ToString(System.Text.StringBuilder sb, Node node, int level)
+	{
+		sb.Append('\t', level);
+		sb.Append(node.name.Remove(0, Application.dataPath.Length));
+		sb.Append('\n');
+		if (node.children != null)
+		{
+			foreach (var child in node.children)
+			{
+				ToString(sb, child, level + 1);
+			}
+		}
+	}
+}
