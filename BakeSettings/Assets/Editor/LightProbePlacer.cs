fc169d12a88e475299657b4efe881a5c2ef48aa7
Hirayama Takashi <hirayama-takashi@kayac.com>
2019-07-05 16:51:02 +0900

    BakeSettings更新

---
+++ BakeSettings/Assets/Editor/LightProbePlacer.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Kayac
+{
+	public class LightProbePlacer : EditorWindow
+	{
+		[MenuItem("Kayac/Rendering/PlaceLightProbes")]
+		static void Create()
+		{
+			GetWindow<LightProbePlacer>(typeof(LightProbePlacer).Name);
+		}
+
+		Vector3 min, max;
+		Vector3Int div;
+
+		void OnGUI()
+		{
+			min = EditorGUILayout.Vector3Field("Min", min);
+			max = EditorGUILayout.Vector3Field("Max", max);
+			div = EditorGUILayout.Vector3IntField("Div", div);
+			if (GUILayout.Button("Generate"))
+			{
+				Generate();
+			}
+		}
+
+		void Generate()
+		{
+			var go = new GameObject("LightProbeGroup");
+			var group = go.AddComponent<LightProbeGroup>();
+			div.x = Mathf.Max(div.x, 1);
+			div.y = Mathf.Max(div.y, 1);
+			div.z = Mathf.Max(div.z, 1);
+
+			var p = new Vector3[(div.x + 1) * (div.y + 1) * (div.z + 1)];
+			var x = min.x;
+			var xStep = (max.x - min.x) / (float)div.x;
+			var yStep = (max.y - min.y) / (float)div.y;
+			var zStep = (max.z - min.z) / (float)div.z;
+			Debug.Log("LightProbe Step:" + xStep + ", " + yStep + ", " + zStep);
+			var i = 0;
+			for (int xi = 0; xi <= div.x; xi++)
+			{
+				var y = min.y;
+				for (int yi = 0; yi <= div.y; yi++)
+				{
+					var z = min.z;
+					for (int zi = 0; zi <= div.z; zi++)
+					{
+						p[i] = new Vector3(x, y, z);
+						i++;
+						z += zStep;
+					}
+					y += yStep;
+				}
+				x += xStep;
+			}
+			group.probePositions = p;
+		}
+	}
+}
---
+++ BakeSettings/Assets/Main.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+	[SerializeField] Transform movingSphere;
+
+	Kayac.FrameTimeWatcher frameTimeWatcher = new Kayac.FrameTimeWatcher();
+
+	void Update()
+	{
+		frameTimeWatcher.Update();
+		var t = Time.time * 1f;
+		var p = new Vector3(
+			4f * Mathf.Cos(t),
+			0.5f,
+			4f * Mathf.Sin(t));
+		movingSphere.localPosition = p;
+	}
+
+	void OnGUI()
+	{
+		var ms = frameTimeWatcher.averageFrameTime / 1000f;
+		GUILayout.Label(ms.ToString("F1"));
+	}
+}
