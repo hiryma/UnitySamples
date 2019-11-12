906cabfbfebf856d7636e7306a067240fa9340db
Hirayama Takashi <hirayama-takashi@kayac.com>
2019-11-12 14:19:04 +0900

    UnloadUnusedAsset

---
+++ UnloadUnusedAssetsTest/Assets/Main.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] RawImage rawImage;
+    int index;
+    int count = TestTextureGenerator.Count;
+    UnityWebRequest req;
+    string message;
+
+    void Update()
+    {
+        if (req == null)
+        {
+            var appDir = System.IO.Path.GetFullPath(".");
+            var url = string.Format("file://{0}/TestData/{1}.png", appDir, index.ToString());
+            Debug.Log(url);
+            message = url;
+            req = UnityWebRequestTexture.GetTexture(url);
+            req.SendWebRequest();
+            index = (index + 1) % count;
+        }
+        else if (req.isDone)
+        {
+            rawImage.texture = DownloadHandlerTexture.GetContent(req);
+            req.Dispose(); // これ忘れちゃダメよ!!(実は最初忘れてた)
+            req = null;
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(message);
+        if (GUILayout.Button("UnloadUnusedAssets"))
+        {
+            Resources.UnloadUnusedAssets();
+        }
+    }
+}
---
+++ UnloadUnusedAssetsTest/Assets/TestTextureGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestTextureGenerator
+{
+    public const int Count = 100;
+#if UNITY_EDITOR
+	[MenuItem("Sample/GenerateTextures")]
+    public static void GenerateTextures()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            GenerateTexture(i);
+        }
+    }
+#endif
+    static void GenerateTexture(int i)
+    {
+        var tex = new Texture2D(4096, 4096);
+        var bytes = ImageConversion.EncodeToPNG(tex);
+		var appDir = System.IO.Path.GetFullPath(".");
+        var path = string.Format("{0}/TestData/{0}.png", appDir, i);
+        System.IO.File.WriteAllBytes(path, bytes);
+        Debug.Log(i);
+    }
+}
