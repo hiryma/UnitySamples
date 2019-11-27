f7b8417ca3f49f9160ee457840c378ad6b6f9c37
Hirayama Takashi <hirayama-takashi@kayac.com>
2019-11-27 17:30:16 +0900

    ComputeShaderTest

---
+++ ComputeShaderTest/Assets/Main.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] RawImage image;
+    [SerializeField] Slider slider;
+    [SerializeField] Text sizeText;
+    [SerializeField] Text fpsText;
+    [SerializeField] Text coresText;
+    [SerializeField] Toggle gpuToggle;
+    [SerializeField] ComputeShader computeShader;
+
+    RenderTexture[] renderTextures;
+    int writeBufferIndex;
+    Texture2D texture2d;
+    Color32[] cpuData;
+    float avgDeltaTime;
+    DateTime prevFrameTime;
+    Kayac.SimpleThreadPool threadPool;
+    int sizeLog;
+    enum Mode
+    {
+        Cpu,
+        Gpu,
+    }
+    Mode mode;
+
+    void Start()
+    {
+        threadPool = new Kayac.SimpleThreadPool();
+        coresText.text = "Core:" + threadPool.threadCount.ToString();
+        mode = Mode.Gpu;
+        Reset();
+        prevFrameTime = DateTime.Now;
+    }
+
+    void Update()
+    {
+        var now = DateTime.Now;
+        var deltaTime = (float)(now - prevFrameTime).Ticks / (float)TimeSpan.TicksPerSecond;
+        prevFrameTime = now;
+        avgDeltaTime *= 0.9f;
+        avgDeltaTime += deltaTime * 0.1f;
+        var fps = 1f / avgDeltaTime;
+        fpsText.text = fps.ToString("F1");
+        bool changed = false;
+        if (gpuToggle.isOn && (mode == Mode.Cpu))
+        {
+            changed = true;
+            mode = Mode.Gpu;
+        }
+        else if (!gpuToggle.isOn && (mode == Mode.Gpu))
+        {
+            changed = true;
+            mode = Mode.Cpu;
+        }
+        var newSizeLog = Mathf.RoundToInt(slider.value);
+        if (newSizeLog != sizeLog)
+        {
+            sizeLog = newSizeLog;
+            changed = true;
+        }
+        if (changed)
+        {
+            Reset();
+        }
+        UpdateTexture();
+    }
+
+    void UpdateTexture()
+    {
+        if (mode == Mode.Cpu)
+        {
+            UpdateTextureCpu();
+        }
+        else if (mode == Mode.Gpu)
+        {
+            UpdateTextureGpu();
+        }
+    }
+
+    static void XorShift32(ref Color32 c)
+    {
+        uint x = ((uint)c.a << 24) | ((uint)c.r << 16) | ((uint)c.g << 8) | c.r;
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+        c.a = (byte)(x >> 24);
+        c.r = (byte)((x >> 16) & 0xff);
+        c.g = (byte)((x >> 8) & 0xff);
+        c.b = (byte)(x & 0xff);
+    }
+
+    static void Job(Color32[] data, int begin, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            XorShift32(ref data[begin + i]);
+        }
+    }
+
+    void UpdateTextureCpu()
+    {
+        const int jobCount = 64;
+        int begin = 0;
+        int rest = cpuData.Length;
+        int unit = (cpuData.Length + jobCount - 1) / jobCount;
+        for (int i = 0; i < jobCount; i++)
+        {
+            int beginCaptured = begin;
+            int countCaptured = (unit <= rest) ? unit : rest;
+            threadPool.AddJob(() => Job(cpuData, beginCaptured, countCaptured));
+            begin += countCaptured;
+        }
+        threadPool.Wait();
+        texture2d.SetPixels32(cpuData);
+        texture2d.Apply();
+        image.texture = texture2d;
+    }
+
+    void UpdateTextureGpu()
+    {
+        var size = 1 << sizeLog;
+        var kernelIndex = computeShader.FindKernel("CSMain");
+        computeShader.SetTexture(kernelIndex, "Source", renderTextures[1 - writeBufferIndex]);
+        computeShader.SetTexture(kernelIndex, "Destination", renderTextures[writeBufferIndex]);
+        uint sizeX, sizeY, sizeZ;
+        computeShader.GetKernelThreadGroupSizes(
+            kernelIndex,
+            out sizeX,
+            out sizeY,
+            out sizeZ
+        );
+        if (SystemInfo.supportsComputeShaders)
+        {
+            computeShader.Dispatch(
+                kernelIndex,
+                (size + (int)sizeX - 1) / (int)sizeX,
+                (size + (int)sizeY - 1) / (int)sizeY,
+                (1 + (int)sizeZ - 1) / (int)sizeZ);
+        }
+        image.texture = renderTextures[writeBufferIndex]; // これ同期待たないとダメでしょ感
+        writeBufferIndex = 1 - writeBufferIndex;
+    }
+
+    void CreateSeedTexture(int size)
+    {
+        texture2d = new Texture2D(size, size, TextureFormat.ARGB32, false);
+        texture2d.name = "2D";
+        cpuData = new Color32[size * size];
+        sizeText.text = size.ToString();
+        for (int i = 0; i < cpuData.Length; i++)
+        {
+            cpuData[i].r = (byte)UnityEngine.Random.Range(0, 256);
+            cpuData[i].g = (byte)UnityEngine.Random.Range(0, 256);
+            cpuData[i].b = (byte)UnityEngine.Random.Range(0, 256);
+            cpuData[i].a = (byte)UnityEngine.Random.Range(0, 256);
+        }
+        texture2d.SetPixels32(cpuData);
+        texture2d.Apply();
+    }
+
+    void Reset()
+    {
+        var size = 1 << sizeLog;
+        CreateSeedTexture(size);
+        if (mode == Mode.Cpu)
+        {
+        }
+        else if (mode == Mode.Gpu)
+        {
+            renderTextures = new RenderTexture[2];
+            for (int i = 0; i < renderTextures.Length; i++)
+            {
+                renderTextures[i] = new RenderTexture(size, size, 1, RenderTextureFormat.ARGB32);
+                renderTextures[i].name = "RT" + i.ToString();
+                renderTextures[i].enableRandomWrite = true;
+                renderTextures[i].Create();
+            }
+            Graphics.Blit(texture2d, renderTextures[1]);
+        }
+    }
+}
---
+++ ComputeShaderTest/Assets/SimpleThreadPool.cs
@@ -0,0 +1,177 @@
+﻿//#define UNITY_WEBGL // WEBGLのテスト
+using System.Threading;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace Kayac
+{
+	// 個別終了待ち合わせできない低機能スレッドプール
+	public class SimpleThreadPool
+	{
+		public SimpleThreadPool(int threadCount = 0, int jobCapacity = 128)
+		{
+			if (threadCount == 0)
+			{
+				var cpuCount = SystemInfo.processorCount;
+				threadCount = System.Math.Min((cpuCount * 3) / 4, cpuCount - 1); //コア数-1か、0.75コア数、の小さい方(雑)
+			}
+#if UNITY_WEBGL
+            threadCount = 0;
+#endif
+			jobStartSemaphore = new Semaphore(0, jobCapacity);
+			jobEndSemaphore = new Semaphore(0, jobCapacity);
+			queue = new Queue<System.Action>();
+			if (threadCount > 0)
+			{
+				threads = new Thread[threadCount];
+				for (int i = 0; i < threadCount; i++)
+				{
+					threads[i] = new Thread(ThreadFunc);
+					threads[i].Priority = System.Threading.ThreadPriority.BelowNormal;
+					threads[i].Start(i);
+				}
+                CreateSamplers(threadCount);
+			}
+            else
+            {
+                // サンプラ一個だけ作る
+                CreateSamplers(1);
+            }
+        }
+
+        void CreateSamplers(int count)
+        {
+            idleSamplers = new CustomSampler[count];
+            jobSamplers = new CustomSampler[count];
+            for (int i = 0; i < count; i++)
+            {
+                idleSamplers[i] = CustomSampler.Create("MyThreadPool.Idle");
+                jobSamplers[i] = CustomSampler.Create("MyThreadPool.Job");
+            }
+        }
+
+        public int threadCount
+        {
+            get
+            {
+                return (threads != null) ? threads.Length : 0;
+            }
+        }
+
+		public void Dispose()
+		{
+			if (threads != null)
+			{
+				for (int i = 0; i < threads.Length; i++)
+				{
+					queue.Enqueue(null); // ダミージョブ投入
+					jobStartSemaphore.Release(); // 投入通知
+				}
+				for (int i = 0; i < threads.Length; i++) // 終了待ち
+				{
+					threads[i].Join();
+				}
+				threads = null;
+			}
+			jobStartSemaphore = null;
+			jobEndSemaphore = null;
+			queue = null;
+		}
+
+		public void AddJob(System.Action job)
+		{
+			Debug.Assert(job != null);
+			lock (queue)
+			{
+				queue.Enqueue(job);
+			}
+			jobStartSemaphore.Release();
+			queuedCount++;
+			if (threads == null) // スレッドないので即実行
+			{
+				TryExecute(0);
+			}
+		}
+
+		public void Wait()
+		{
+			// 終了待ち。終了済みの数がスレッド数になるまで待つ
+			for (int i = 0; i < queuedCount; i++)
+			{
+				jobEndSemaphore.WaitOne();
+			}
+			queuedCount = 0;
+		}
+
+		public bool completed
+		{
+			get
+			{
+				// 終了待ち。終了済みの数がスレッド数になるまで待つ
+				for (int i = 0; i < queuedCount; i++)
+				{
+					if (jobEndSemaphore.WaitOne(0))
+					{
+						queuedCount--;
+					}
+					else
+					{
+						break;
+					}
+				}
+				return (queuedCount == 0);
+			}
+		}
+		// non public -------------------
+
+		void ThreadFunc(object arg)
+		{
+			int index = (int)arg;
+			Profiler.BeginThreadProfiling("MyThreadPool", "MyThread: " + index.ToString());
+			while (true) // 終了要求が来ていなければ先へ。タイムアウトを0にすれば待たずに抜けられる。
+			{
+				if (!TryExecute(index))
+				{
+					break;
+				}
+			}
+			Profiler.EndThreadProfiling();
+		}
+
+		bool TryExecute(int index)
+		{
+			idleSamplers[index].Begin();
+			jobStartSemaphore.WaitOne(); // ジョブが投入されるまで待つ
+			idleSamplers[index].End();
+			System.Action job = null;
+			lock (queue)
+			{
+				job = queue.Dequeue();
+			}
+			if (job == null) // 終了のダミージョブ
+			{
+				return false;
+			}
+			try
+			{
+				jobSamplers[index].Begin();
+				job();
+				jobSamplers[index].End();
+			}
+			catch (System.Exception e)
+			{
+				Debug.Log("Job Threw Exception: " + e.GetType().Name);
+			}
+			jobEndSemaphore.Release();
+			return true;
+		}
+		Semaphore jobStartSemaphore;
+		Semaphore jobEndSemaphore;
+		Thread[] threads;
+		Queue<System.Action> queue;
+		int queuedCount;
+		CustomSampler[] idleSamplers;
+		CustomSampler[] jobSamplers;
+	}
+}
