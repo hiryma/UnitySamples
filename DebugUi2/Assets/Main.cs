0beb819c6c195efbb3c022dd2561d04e976c4362
Hirayama Takashi <hirayama-takashi@kayac.com>
2023-03-17 10:55:26 +0900

    コントロール増やした

--- DebugUi2/Assets/Main.cs
+++ DebugUi2/Assets/Main.cs
@@ -19,15 +19,24 @@ public class Main : MonoBehaviour
 
         menu = new Menu(100, 40);
         var subA = new SubMenu("SubA", 100, 40, Direction.Down);
-        subA.AddItem("A1", () => Debug.Log("A1"));
-        subA.AddItem("A2", () => Debug.Log("A2"));
-        var subB = new SubMenu("SubA", 100, 40, Direction.Down);
-        subB.AddItem("B1", () => Debug.Log("B1"));
-        subB.AddItem("B2", () => Debug.Log("B2"));
+
+        var subB = new SubMenu("SubA", 100, 40, Direction.Right);
+        for (var i = 0; i < 10; i++)
+        {
+            subB.AddItem("B" + i.ToString(), () => Debug.Log("B" + i.ToString()));
+        }
+
         subA.AddSubMenu(subB, Direction.Right);
+        for (var i = 0; i < 10; i++)
+        {
+            subA.AddItem("A" + i.ToString(), () => Debug.Log("A" + i.ToString()));
+        }
+
         menu.AddSubMenu(subA, Direction.Down);
-        menu.AddItem("1", () => Debug.Log("1"));
-        menu.AddItem("2", () => Debug.Log("2"));
+        for (var i = 0; i < 10; i++)
+        {
+            menu.AddItem(i.ToString(), () => Debug.Log(i.ToString()));
+        }
         manager.Add(menu, 0, 0);
     }
 }
